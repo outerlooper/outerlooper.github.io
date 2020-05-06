3c47a45fa199628c512e6167c433a956ef28efdc
Michael Cooper <mike@Outerlooper-MacMini.local>
2020-05-06 14:27:37 -0500

    Initial Copy from Source Build

---
+++ precache-manifest.7fec436bfdaed5b36572c6c1f76936ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80907fe4ca88d09694106a53ea4978bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c555ea2650a26ba417e6",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3e4a702c4346ee6288ba",
+    "url": "/static/js/2.9c599f99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9c599f99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c555ea2650a26ba417e6",
+    "url": "/static/js/main.ea1a736a.chunk.js"
+  },
+  {
+    "revision": "03eec2d3e199a22d30eb",
+    "url": "/static/js/runtime-main.fd9d7532.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
